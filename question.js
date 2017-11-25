5a235c71ad9139c9965f651b79c726878e555fe4
Esther Liao <erliao@princeton.edu>
2017-11-25 03:20:43 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,22 @@
-'use strict';
+class Question {
+  constructor(Content) {
+    this.content = Content;
+    this.save();
+    // Question._All.push(this.question)
+  }
+
+  save(){
+    this.constructor._All.push(this)
+  }
+
+  static All() {
+    return this._All;
+  }
+
+  static Find(id) {
+    console.log(this._All);
+    return this._All[id - 1];
+  }
+}
+
+Question._All = [];
